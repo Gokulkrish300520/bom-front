1a0eaf6897aaffd568edf96a282986e1ae17e332
Gokulkrish300520 <gokul.jayakumar2005@gmail.com>
2025-09-16 11:27:12 +0530

    removed viloation key

--- server/server/settings.py
+++ server/server/settings.py
@@ -244,5 +244,10 @@ LOGGING = {
     "root": {
         "handlers": ["console"],
         "level": "ERROR",
-    },
+    }
 }
+
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = "glonix"
+AWS_S3_REGION_NAME = "us-east-1"
