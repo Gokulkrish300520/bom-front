67dc58e9550a55519390b35ca0aa60b20e786015
Gokulkrish300520 <gokul.jayakumar2005@gmail.com>
2025-10-21 18:51:08 +0530

    update refresh token rotation logic

--- server/server/settings.py
+++ server/server/settings.py
@@ -224,7 +224,7 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
     "REFRESH_TOKEN_LIFETIME": timedelta(minutes=15),
-    "ROTATE_REFRESH_TOKENS": False,
+    "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "AUTH_HEADER_TYPES": ("Bearer",),
     "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
