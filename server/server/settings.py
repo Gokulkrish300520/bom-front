85717b9a2f0725624dc16e6f5e6f96b1df5e5a78
Gokulkrish300520 <gokul.jayakumar2005@gmail.com>
2025-10-26 18:11:08 +0530

    deleted tables

--- server/server/settings.py
+++ server/server/settings.py
@@ -89,7 +89,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://bom-front.vercel.app",
     "https://bom-front-production.up.railway.app",
     "https://bom-front-production-140a.up.railway.app",
-    "http://web-production-6baf3.up.railway.app"
+    "https://web-production-6baf3.up.railway.app"
     # Add other addresses if needed
 ]
 
@@ -174,7 +174,7 @@ CSRF_TRUSTED_ORIGINS = [
     "https://bom-front-production.up.railway.app",
     "https://bom-front-production-140a.up.railway.app",
     "https://bom-front-production-140a.up.railway.app",
-    "http://web-production-6baf3.up.railway.app",
+    "https://web-production-6baf3.up.railway.app",
     "https://bom-front-dgh6-git-feature-backend-herpulse.vercel.app"
 ]
 
